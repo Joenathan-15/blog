711061d2e2bed9f940e381662adc80251fab5061
Joenathan <112812440+Joenathan-15@users.noreply.github.com>
2023-12-04 01:10:45 +0700

    fix:sidebar configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,9 +70,9 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'DocsSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
